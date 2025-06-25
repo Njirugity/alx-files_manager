a847c497e2bdd42a2adca77a1e7b8d487ce2caf3
brian <muriithibrian283@gmail.com>
2025-06-25 21:20:18 +0300

    File manager

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 router.get('/status', AppController.getStatus);
@@ -10,6 +11,10 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 
 export default router;
